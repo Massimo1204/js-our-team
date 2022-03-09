6baf08b8e911106d293fbd8e8456c5e16c001011
Massimo1204 <xiangmassimo@gmail.com>
2022-03-09 17:10:25 +0100

    add member feature

--- js/main.js
+++ js/main.js
@@ -32,10 +32,41 @@ const team = [
 ];
 
 const content = document.getElementById('main-content');
+const inputName = document.getElementById('name');
+const inputRole = document.getElementById('role');
+const submit = document.getElementById('submit');
+const message = document.getElementById('alert');
+let newMembersCounter = 0;
 
-for ( let i=0; i<team.length ; i++){
-    const newCard = document.createElement('div')
-    newCard.classList.add('col-4','mt-5');
-    newCard.innerHTML = "<img src='img/"+team[i].image+"' class='img-fluid'> <p class='mt-3 fs-4'>"+team[i].name+"</p> <p>"+team[i].role+"</p>";
-    content.append(newCard);
+function displayMembers( team,content){
+    content.innerHTML='';
+    for ( let i=0; i<team.length ; i++){
+        const newCard = document.createElement('div')
+        newCard.classList.add('col-4','mt-5');
+        newCard.innerHTML = "<img src='img/"+team[i].image+"' class='img-fluid'> <p class='mt-3 fs-4'>"+team[i].name+"</p> <p>"+team[i].role+"</p>";
+        content.append(newCard);
+    }
 }
+
+displayMembers(team,content);
+
+submit.addEventListener('click',function(){
+    message.innerHTML='';
+    newMembersCounter++;
+    if(newMembersCounter>0 && newMembersCounter<5){
+        if(inputName.value =='' || inputRole.value == ''){
+            message.innerHTML = "Don't leave Name or Role blank please.";
+            newMembersCounter--;
+        }else{
+            const member = {
+                name : inputName.value,
+                role : inputRole.value,
+                image : 'new-team-member-0'+newMembersCounter+'.jpg',
+            }
+            team.push(member);
+        }
+    }else if(newMembersCounter>=5){
+        message.innerHTML = 'Sorry! The team is full.';
+    }
+    displayMembers(team,content);
+});
